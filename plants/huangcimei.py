5e9eed5a3206b06898102c88c19190433d2b0dfd
彭子隽 <2200012909@stu.pku.edu.cn>
2025-04-30 01:23:22 +0800

    Fix: update last attack time in huangcimei

--- plants/huangcimei.py
+++ plants/huangcimei.py
@@ -8,6 +8,7 @@ class HuangCiMei(Plant):
         self.last_attack_time = tick
     
     def attack(self, status, harm_value):
+        self.last_attack_time = status.global_ticks
         for i in range(self.index[0] - 1, self.index[0] + 2):
             for j in range(self.index[1] - 1, self.index[1] + 2):
                 if i < 9 and i >= 0 and j < 5 and j >= 0:
@@ -22,7 +23,7 @@ class HuangCiMei(Plant):
             if status.global_ticks - self.start_tick >= 1800:
                 self.status = 2
             if status.global_ticks - self.last_attack_time >= self.attack_interval:
-                self.attack(status, 10)
+                self.attack(status, 30)
         elif self.status == 2 and status.global_ticks - self.last_attack_time >= self.attack_interval:
-            self.attack(status, 30)
+            self.attack(status, 50)
         return super().check_life(event, status)
